86686b0043eb3798ad594a20b74e5677e5b391dd
SoldierAb <ab140140@163.com>
2019-05-01 15:22:39 +0800

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+    extends: [
+        'eslint-config-alloy/typescript',
+    ],
+    globals: {
+        // 这里填入你的项目需要的全局变量
+        // 这里值为 false 表示这个全局变量不允许被重新赋值，比如：
+        //
+        // jQuery: false,
+        // $: false
+    },
+    parser: 'typescript-eslint-parser',
+    plugins: [
+        'react',
+        'react-native',
+        'typescript'
+    ],
+    rules: {
+        // @fixable 必须使用 === 或 !==，禁止使用 == 或 !=，与 null 比较时除外
+        'eqeqeq': [
+            'error',
+            'always',
+            {
+                null: 'ignore'
+            }
+        ],
+        // 类和接口的命名必须遵守帕斯卡命名法，比如 PersianCat
+        'typescript/class-name-casing': 'error'
+    }
+}
--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, View,Alert} from 'react-native';
+import {Platform,View} from 'react-native';
 import Counter from './lib/src/modules/Counter';
 
 const instructions = Platform.select({
@@ -34,13 +34,13 @@ export default class App extends Component<Props> {
   }
 
   render() {
-    const {clickDec,clickIncre} =this;
+    const {clickDec,clickIncre} = this;
     return (
       <View style={{flex: 1,flexDirection:'row', alignItems:'center',justifyContent:'space-between',flexWrap:'wrap'}}>
             <View style={{width: '30%', height: 50, backgroundColor: 'powderblue'}} />
             <View style={{width: '30%', height: 50, backgroundColor: 'green'}} />
             <View style={{width: '30%', height: 50, backgroundColor: 'yellow'}} />
-            <Counter count={this.state.count} onInc={clickIncre} onDec={clickDec} />
+            <Counter name="计数器" count={this.state.count} onInc={clickIncre} onDec={clickDec} />
       </View>
     );
   }
